678a2d1c44123bd7a384d40752a4d1d94b4d0a5a
TheFloAnd <holst568@gmail.com>
2021-11-15 08:05:38 +0100

    jquery

--- resources/layout/template.php
+++ resources/layout/template.php
@@ -39,13 +39,9 @@
       })
     </script>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-            crossorigin="anonymous"></script>
+    <script src="/resources/js/jquery-3.3.1.min.js"></script>
 
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-            integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-            crossorigin="anonymous"></script>
+    <script src="/resources/js/jquery-ui.min.js"></script>
     <script src="/resources/js/select2.full.min.js"></script>
     <script>
       $(document).ready(function() {
