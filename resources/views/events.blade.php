cffa18118f61f05985808a4aa49dfa89d4b40d01
TheFloAnd <holst568@gmail.com>
2021-11-19 14:44:45 +0100

    fix?

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -251,37 +251,37 @@ $events = events::index();
                                         '. $row['room'] .'
                                       </td>';
                                     if(strftime('%d.%m.%Y', strtotime($row['start'])) != strftime('%d.%m.%Y', strtotime($row['end']))){
-          
-                                      if(strftime('%H:%M', strtotime($row['start'])) == '00:00'){
-          echo'<td class="table_search">'. strftime('%d.%m.%Y %H:%M', strtotime($row['start'])) .'</td>';
-                                      }else{
-          echo'<td class="table_search">'. strftime('%d.%m.%Y %H:%M', strtotime($row['start'])) .'</td>';
-                                      }
-                                      if(strftime('%H:%M', strtotime($row['end'])) == '00:00'){
-          echo'<td class="table_search">'. strftime('%d.%m.%Y', strtotime($row['end'])) .'</td>';
-                                      }else{
-          echo'<td class="table_search">'. strftime('%d.%m.%Y %H:%M', strtotime($row['end'])) .'</td>';
-                                      }
+                                    
+                                    if(strftime('%H:%M', strtotime($row['start'])) == '00:00'){
+                                    echo'<td>'. strftime('%d.%m.%Y', strtotime($row['start'])) .'</td>';
+                                    }else{
+                                    echo'<td>'. strftime('%d.%m.%Y - %H:%M', strtotime($row['start'])) .'</td>';
+                                    }
+                                    if(strftime('%H:%M', strtotime($row['end'])) == '00:00'){
+                                    echo'<td>'. strftime('%d.%m.%Y ', strtotime($row['end'])) .'</td>';
+                                    }else{
+                                    echo'<td>'. strftime('%d.%m.%Y - %H:%M', strtotime($row['end'])) .'</td>';
+                                    }
                                     }
                                     if(strftime('%d.%m.%Y', strtotime($row['start'])) == strftime('%d.%m.%Y', strtotime($row['end']))){
                                     if(strftime('%H:%M', strtotime($row['start'])) == strftime('%H:%M', strtotime($row['end']))){
                                     
                                     if(strftime('%H:%M', strtotime($row['start'])) == '00:00'){
-          echo'<td class="table_search" colspan="2">'. strftime('%d.%m.%Y ', strtotime($row['start'])) .'</td><td style="display:none;"></td>';
+                                    echo'<td colspan="2">'. strftime('%d.%m.%Y ', strtotime($row['start'])) .'</td><td style="display:none;">';
                                     }else{
-          echo'<td class="table_search" colspan="2">'. strftime('%d.%m.%Y - %H:%M', strtotime($row['start'])) .'</td><td style="display:none;"></td>';
+                                    echo'<td colspan="2">'. strftime('%d.%m.%Y - %H:%M', strtotime($row['start'])) .'</td><td style="display:none;">';
                                     }
                                     }
                                     if(strftime('%H:%M', strtotime($row['start'])) != strftime('%H:%M', strtotime($row['end']))){
                                     if(strftime('%H:%M', strtotime($row['start'])) == '00:00'){
-          echo'<td class="table_search">'. strftime('%d.%m.%Y', strtotime($row['start'])) .'</td>';
+                                    echo'<td>'. strftime('%d.%m.%Y', strtotime($row['start'])) .'</td>';
                                     }else{
-          echo'<td class="table_search">'. strftime('%d.%m.%Y - %H:%M', strtotime($row['start'])) .'</td>';
+                                    echo'<td>'. strftime('%d.%m.%Y - %H:%M', strtotime($row['start'])) .'</td>';
                                     }
                                     if(strftime('%H:%M', strtotime($row['end'])) == '00:00'){
-          echo'<td class="table_search">'. strftime('%d.%m.%Y', strtotime($row['end'])) .'</td>';
+                                    echo'<td>'. strftime('%d.%m.%Y', strtotime($row['end'])) .'</td>';
                                     }else{
-          echo'<td class="table_search">'. strftime('%H:%M', strtotime($row['end'])) .'</td>';
+                                    echo'<td>'. strftime('%H:%M', strtotime($row['end'])) .'</td>';
                                     }
                                     }
                                     }
