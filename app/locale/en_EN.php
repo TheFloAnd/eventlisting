90bdf9f9fde973a6655af0e25ef534232e1d032b
thefloand <holst568@gmail.com>
2021-12-10 11:42:21 +0000

    fix

--- app/locale/en_EN.php
+++ app/locale/en_EN.php
@@ -2,7 +2,7 @@
 
 $lang = array();
 
-$lang['nav']          = 'Navigation';
+$lang['nav']            = 'Navigation';
 $lang['index']          = 'Main';
 $lang['event']          = 'Event';
 $lang['events']         = 'Events';
@@ -26,7 +26,7 @@ $lang['alias']          = 'Alias';
 $lang['color']          = 'Color';
 
 $lang['preview']        = 'Preview';
-$lang['unit']        = 'Unit';
+$lang['unit']           = 'Unit';
 
 $lang['room']           = 'Room';
 $lang['start']          = 'Start';
@@ -35,7 +35,7 @@ $lang['date']           = 'Date';
 
 $lang['project']        = 'Project';
 $lang['from']           = 'From';
-$lang['till']           = 'until';
+$lang['till']           = 'Until';
 $lang['remaining_days'] = 'In';
 $lang['repeat']         = 'Repeats';
 $lang['not_applicable'] = 'Not Applcable';
@@ -57,27 +57,27 @@ $lang['seconds']        = 'Seconds';
 $lang['database']        = 'Database';
 $lang['database-table']  = 'Databasetabel';
 
-$lang['no-repeat']        = 'No Repeats';
-$lang['toggle-column']        = 'Toggle Column';
+$lang['no-repeat']       = 'No Repeats';
+$lang['toggle-column']   = 'Toggle Column';
 
 // Error
 
 
-$lang['invalide-event-input']       = 'Please set an Event name (max. 50 symbols)!';
-$lang['invalide-group-input']       = 'Please add one or more Groups!';
-$lang['invalide-room-input']        = 'The max. length is 10 Symbols';
+$lang['invalide-event-input']               = 'Please set an Event name (max. 50 symbols)!';
+$lang['invalide-group-input']               = 'Please add one or more Groups!';
+$lang['invalide-room-input']                = 'The max. length is 10 Symbols';
 
 
-$lang['invalide-group_name-input']        = 'Please set a group name (max. 100 symbols)!';
-$lang['invalide-group_alias-input']       = 'Please set a group alias (max. 10 symbols)!';
+$lang['invalide-group_name-input']          = 'Please set a group name (max. 100 symbols)!';
+$lang['invalide-group_alias-input']         = 'Please set a group alias (max. 10 symbols)!';
 
-$lang['invalide-settings_value-input']  = 'Please set a valid value (max. 50 symbols)!';
+$lang['invalide-settings_value-input']      = 'Please set a valid value (max. 50 symbols)!';
 
 // Datatable
-$lang['datatable_lang-file']        = '';
+$lang['datatable_lang-file']                = '';
 
 
 // delete Table
 
-$lang['delete_table']        = 'Do you really want to delete/empty the Databasetable?';
-$lang['delete_table-info']        = 'If the table Groups is reset, the table events will also be reset!';
+$lang['delete_table']                       = 'Do you really want to delete/empty the Databasetable?';
+$lang['delete_table-info']                  = 'If the table Groups is reset, the table events will also be reset!';
